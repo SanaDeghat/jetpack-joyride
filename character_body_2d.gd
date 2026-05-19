bcc24168d65d08bd1aa2682a8126a6b1df00bfbd
Sana Deghat <Sana.Deghat@gmail.com>
2026-05-18 20:49:33 -0700

    floor and roof

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+const roof_y=50;
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,8 +11,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept") :
 		velocity.y = JUMP_VELOCITY
 	elif position.y<600:
-		print (position.y)
-
+		
 		velocity += get_gravity() * delta
 	else: 
 		velocity.y=0
@@ -23,5 +22,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	print (position.y)
 
 	move_and_slide()
+	if position.y < roof_y:
+		position.y = roof_y
+		velocity.y = 0
